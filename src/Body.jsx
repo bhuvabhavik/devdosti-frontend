48140960050b5269ee0016935385a36dfee9d07c
bhuvabhavik <bhavikbhuva80@gmail.com>
2025-05-29 14:04:45 +0530

    footer router

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,4 +1,5 @@
 
+import Footer from './Footer'
 import NavBar from './NavBar'
 import { Outlet } from 'react-router-dom'
 
@@ -7,6 +8,7 @@ const Body = () => {
     <>
        <NavBar/>
        <Outlet/>
+       <Footer/>
        </>
 
   )
