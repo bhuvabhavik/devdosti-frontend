85d4656f3c0cec2ecc7a5ce1fad3cf2c688db9dd
bhuvabhavik <bhavikbhuva80@gmail.com>
2025-06-01 19:29:07 +0530

    redirect to login if not authenticated

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,17 +1,48 @@
+import { useDispatch, useSelector } from "react-redux";
+import { BASE_URL } from "../utils/constants";
+import Footer from "./Footer";
+import NavBar from "./NavBar";
+import { Outlet, useNavigate } from "react-router-dom";
+import { addUser } from "../utils/userSlice";
+import { useEffect } from "react";
+import axios from "axios";
 
-import Footer from './Footer'
-import NavBar from './NavBar'
-import { Outlet } from 'react-router-dom'
+const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-const Body = () => { 
+  const userData = useSelector((store) => store.user);
+
+  const fetchUser = async () => {
+    if (userData) {
+      return;
+    }
+
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+
+      dispatch(addUser(res.data));
+    } catch (err) {
+      if (err.status === 401) {
+        navigate("/login");
+      }
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <>
-       <NavBar/>
-       <Outlet/>
-       <Footer/>
-       </>
+      <NavBar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
 
-  )
-}
-
-export default Body
+export default Body;
